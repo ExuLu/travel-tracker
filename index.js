776362d84b9f25407e86eeb99f87a240468dd45b
Alena Sharai <exultantislupus@gmail.com>
2026-02-03 10:52:11 +0000

    feat: setup database

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
-import express from "express";
-import bodyParser from "body-parser";
+import express from 'express';
+import bodyParser from 'body-parser';
+import pg from 'pg';
+import * as dotenv from 'dotenv';
+
+dotenv.configDotenv();
+
+const db = new pg.Client({
+  user: 'postgres',
+  password: process.env.PG_DB_PASSWORD,
+  host: 'localhost',
+  port: 5432,
+  database: 'world',
+});
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-app.get("/", async (req, res) => {
+app.get('/', async (req, res) => {
   //Write your code here.
 });
 
